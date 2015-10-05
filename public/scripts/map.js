d6c0d8260041282b6ccb1b072ddba0ece846f330
Joshua Swiss <swissjoshua@gmail.com>
2015-10-05 19:24:33 +0100

    search map added and styled

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,13 +1,173 @@
+var View = View || {};
 
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
 
-function initMap() {
-  // Create a map object and specify the DOM element for display.
+function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('googlemap'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
+    center: {lat: 51.5286416, lng: -0.1015987},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: styledArray
   });
-  console.log('Map')
-}
 
-console.log('Hey')
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+ }
+	 	var styledArray =  [
+	    {
+	        "featureType": "landscape",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "color": "#e8e8e8"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "landscape.natural",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "color": "#e0efef"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "poi",
+	        "elementType": "geometry.fill",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "hue": "#1900ff"
+	            },
+	            {
+	                "color": "#c0e8e8"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "lightness": 100
+	            },
+	            {
+	                "visibility": "simplified"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "labels",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "transit.line",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "lightness": 700
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "color": "#00c1df"
+	            }
+	        ]
+	    }
+	];
+
+
+
+
+// View = {
+// 	search: function() {
+// 		$('#map-search').on('submit', function(e) {
+// 			e.preventDefault();
+// 			e.stopPropagation();
+// 			var locationSearch = $('#map-search').val();
+// 			console.log(locationSearch)
+// 			$.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locationSearch + 'UK', function(response) {
+// 			// render(response);
+// 			console.log(response)
+// 			})
+// 		})
+// 	}
+
+// function render(response) {
+// 		console.log(response);
+// 	}
+
+
