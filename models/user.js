88594225180da70cd2c3e27b0e47268f7f268dcf
Joshua Swiss <swissjoshua@gmail.com>
2015-10-07 09:30:57 +0100

    generated new api keys and added them to zshrc

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema ({
-	userId: String,
-	username: String,
-	password: String,
-	email: String,
-	location: String,
-	photos: String
+	userId:      String,
+	token:       String,
+	displayName: String,
+	username:    String,
+	password:    String,
+	email:       String,
+	location:    String,
+	photos:      String
 });
 
 var User = mongoose.model('User', userSchema);
